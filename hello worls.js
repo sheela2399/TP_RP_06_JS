01fd483b8aadbc993bc446a015b6c4dbc5df58ab
sheela mishra <sheelamishra1999@gmail.com>
2024-04-25 19:37:26 +0530

    hello world task added

---
+++ hello worls.js
@@ -0,0 +1,17 @@
+console.log ("hello world")
+
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+console.log("hello world")
+
+let a =5; b=4; x=a+b;
+console.log(x)
+
+console.log(5+2)
+console.log(3+1)
+console.log(11+2)
+console.log(12+1)
+
+
