18a86a947c5d5f6a22bf94d554318511710c11ca
sheela mishra <sheelamishra1999@gmail.com>
2024-04-26 16:10:21 +0530

    Functions added

--- function.js
+++ function.js
@@ -1,4 +1,32 @@
+//1. greeting function
 function greetings (name){
      console.log(`Hello,${name}`); 
 }
 greetings ("sheela")
+
+
+//2. Area of Reactangle
+function Area(length,width){
+     let A= length*width;
+     console.log(A)
+}
+Area(5,5)
+
+
+//3. Temperature converter celcius to ferenheit
+function celciusConverter(celcTemp){
+     let TemperatureInFeren = (celcTemp * 9/5) + 32 ;
+     console.log(TemperatureInFeren)
+}
+celciusConverter(40)
+
+//4. Temperature converter ferenheit to celcius 
+function ferenheitConverter(ferenTemp){
+     let TemperatureInCel=(ferenTemp - 32) * 5/9 ;
+     console.log(TemperatureInCel)
+}
+ferenheitConverter(108)
+
+
+
+
