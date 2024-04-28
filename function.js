55f4e2823c7782096c87e738c6a15fba927bf8df
sheela mishra <sheelamishra1999@gmail.com>
2024-04-28 15:01:55 +0530

    changes done

--- function.js
+++ function.js
@@ -29,4 +29,35 @@ fahrenheitToCelcius(108)
 
 
 
+// Calculator operation functions
+// 1.Addition
+function addition(a,b){
+     let add = a+b;
+     console.log(add)
+     }
+     addition (4,5)
+ 
+     // 2.substraction
+     function substraction(a,b){
+     let subs = a-b;
+     console.log(subs)
+     }
+     addition (10,2)
+
+     // 3.multiplication
+     function multiplication(a,b){
+         let multiply = a*b;
+         console.log(multiply)
+     }
+     multiplication (3,2)
+
+     // 4.Division
+     function division(a,b){
+         let divide = a/b;
+         console.log(divide)
+     }
+     division(8,2)
+ 
+
+
 
